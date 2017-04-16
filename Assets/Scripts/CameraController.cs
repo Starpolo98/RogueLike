24635e49d7168080843e0400e0c60980521c629b
Starpolo98 <paul.lucas@epfl.ch>
2017-04-16 15:00:07 +0200

    Add files via upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public PlayerController thePlayer;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = new Vector3(thePlayer.transform.position.x, thePlayer.transform.position.y, -10f);
+        transform.position = thePlayer.transform.position + new Vector3(0f, 0f, -10);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public Animator myAnimator;
+    public GameObject Projectile;
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxisRaw("Horizontal") > 0f || Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal")*moveSpeed*Time.deltaTime,0f,0f));
+        }
+        if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < 0f)
+        {
+            transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            
+        }
+
+        myAnimator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        myAnimator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+    }
+}
